3c4882379ef2655b37470c66dfa1672825511fa2
yilmazburak0 <yilmazburak1210@gmail.com>
2025-04-30 13:13:11 +0300

    feat: complete initial UI for Gemini clone

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets';
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello</span>
+          </p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beatiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Tell me about React js and React native</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input
+              type="text"
+              placeholder="Enter a prompt here"
+              name=""
+              id=""
+            />
+            <div className='icons'>
+              <img src={assets.gallery_icon}></img>
+              <img src={assets.mic_icon}></img>
+              <img src={assets.send_icon}></img>
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info, including about people, so
+            double-check its responses. Your privacy and Gemini Apps
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main
