2c380630b85475457419f0ea196be9b7f2c58db6
yilmazburak0 <yilmazburak1210@gmail.com>
2025-04-30 16:58:07 +0300

    feat: enhance result handling and loading state in context provider

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets';
+import { Context } from '../../context/ContextStore';
 
 const Main = () => {
+
+  const {
+    onSent,recentPrompt,showResult,resultData,input,setInput,loading
+  } = useContext(Context);
+
   return (
     <div className="main">
       <div className="nav">
@@ -10,42 +16,68 @@ const Main = () => {
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
-        <div className="greet">
-          <p>
-            <span>Hello</span>
-          </p>
-          <p>How can I help you today?</p>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <p>Suggest beatiful places to see on an upcoming road trip</p>
-            <img src={assets.compass_icon} alt="" />
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello</span>
+              </p>
+              <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest beatiful places to see on an upcoming road trip</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Briefly summarize this concept: urban planning</p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Brainstorm team bonding activities for our work retreat</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Tell me about React js and React native</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
           </div>
-          <div className="card">
-            <p>Briefly summarize this concept: urban planning</p>
-            <img src={assets.bulb_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Brainstorm team bonding activities for our work retreat</p>
-            <img src={assets.message_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Tell me about React js and React native</p>
-            <img src={assets.code_icon} alt="" />
-          </div>
-        </div>
+        )}
+
         <div className="main-bottom">
           <div className="search-box">
             <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
               type="text"
               placeholder="Enter a prompt here"
               name=""
               id=""
             />
-            <div className='icons'>
+            <div className="icons">
               <img src={assets.gallery_icon}></img>
               <img src={assets.mic_icon}></img>
-              <img src={assets.send_icon}></img>
+              <img onClick={() => onSent()} src={assets.send_icon}></img>
             </div>
           </div>
           <p className="bottom-info">
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -7,13 +7,42 @@ const ContextProvider = (props) => {
     const [input, setInput] = useState("");
     const [recentPrompt, setRecentPrompt] = useState("");
     const [prevPrompts, setprevPrompt] = useState([]);
-    const [showResult] = useState(false);
-    const [loading] = useState(false);
-    const [resultData] = useState("");
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("");
 
+    const delayPara = (index,nextWord) => {
+        setTimeout(function() {
+            setResultData((prev) => prev + nextWord);
+        }, 75*index);
+    }
 
-    const onSent = async (prompt) => {
-        await main(prompt)
+    const onSent = async () => {
+        setResultData("");
+        setLoading(true);
+        setShowResult(true);
+        setRecentPrompt(input);
+        const response = await main(input);
+        let responseArray = response.split("**");
+        let newResponse ;
+        for (let i = 0; i < responseArray.length; i++) {
+            if (i === 0 || i%2 !== 1) {
+                newResponse += responseArray[i];
+            }
+            else{
+                newResponse += "<b>"+responseArray[i]+"</b>";
+            }
+            
+        }
+        let newResponse2 = newResponse.split("*").join("<br/>");
+        let newResponseArray = newResponse2.split(" ");
+        for (let i = 0; i < newResponseArray.length; i++) {
+            const nextWord = newResponseArray[i];
+            delayPara(i,nextWord+" ");
+            
+        }
+        setLoading(false); 
+        setInput("");
     }
 
 
